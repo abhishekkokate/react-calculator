d747e06c1f888386c560999e77256c1e54bec73b
Abhishek Kokate <abhishekkokate8@gmail.com>
2022-01-22 17:01:41 +0530

    github link added

--- src/App.js
+++ src/App.js
@@ -138,6 +138,9 @@ function App() {
 
   return (
     <div className="app">
+      <div className="links">
+        <a href="https://github.com/abhishekkokate/react-calculator" target="_blank"> <p>GitHub</p> <img src="https://img.icons8.com/glyph-neue/64/000000/github.png"/></a>
+      </div>
       <div className="calculator-grid">
         <div className="output">
           <div className="prev-operand">{formatOperand(prevOperand)} {operation}</div>
